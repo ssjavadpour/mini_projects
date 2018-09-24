e7dd211e4df2a03b616a9b63da8d8144be2e3584
sepidj <sepi@2160consulting.com>
2018-09-24 16:25:54 -0400

    solve the puzzle mini project

---
+++ solve_the_puzzle.py
@@ -0,0 +1,7 @@
+#The Problem :
+# rename files in "Prank" folder by removing all digits in the file name
+#*****************************************************************************
+
+import os
+
+#os.path.open("file_path")
